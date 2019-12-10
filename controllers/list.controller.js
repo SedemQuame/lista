699d6b333d7555102dfd2a5c5db4ce2cbaf7b88f
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-12-10 22:07:56 +0000

    lista project restructuring.

---
+++ controllers/list.controller.js
@@ -0,0 +1,23 @@
+// jshint esversion:6
+//====================================== requiring modules ===========================================//
+const Note = require('../models/list.models');
+
+//================================== creating HTTP handler methods ==================================//
+
+// create and save new list item
+exports.create = (req, res) => {};
+
+// return all db list items
+exports.findAll = (req, res) => {};
+
+// return a db list items
+exports.findOne = (req, res) => {};
+
+// update a db list items
+exports.updateOne = (req, res) => {};
+
+// delete all db list items
+exports.deleteAll = (req, res) => {};
+
+// delete a db list items
+exports.deleteOne = (req, res) => {};
